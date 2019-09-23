ac3cda202fd37d07ea8a49cf38f833642ce8afbe
akolli1972 <kolli_aruna2006@yahoo.co.in>
2019-09-23 12:38:43 -0400

    Add files via upload

---
+++ week6-triviagame/assets1/javascript1/app1.js
@@ -0,0 +1,210 @@
+$(document).ready(function () {
+    var options = [
+        {
+            question: "What does the term 'Deuce' mean in Tennis?",
+            choice: ["40-40", "30-15", "15-30", "0-40"],
+            answer: 0,
+            
+         },
+         {
+             question: "Who won womens singles US OPEN 2019?", 
+            choice: ["Serena", "Andreescu", "Kerber", "Halep"],
+            answer: 1,
+            
+         }, 
+         {
+             question: "Who won Mens singles US OPEN, 2019?", 
+            choice: ["Federer", "Djokovich", "Nadal", "Thiem" ],
+            answer: 2,
+            
+        }, 
+        {
+            question: "French Open is played on what surface?", 
+            choice: ["Hard Court", "Grass" , "Clay" , "glass" ],
+            answer: 2,
+            
+        }, 
+        {
+            question: "The Grand Slam tennis played on grass is ", 
+            choice: ["Australian", "US OPEN", "French Open", "Wimbledon" ],
+            answer: 3,
+            
+        }, 
+        {
+            question: "Who among these tennis palyers belong to the US?", 
+            choice: ["Andy Roddick", " Stan Wawrinka", "Nick Kyrios", "Andy Murry" ],
+            answer: 0,
+            
+        }, 
+        {
+            question: "Who won 24 grand slams in Womens Singles tennis so far?", 
+            choice: ["Serena", "Margaret Court", "Steffi Graff" , "Halep" ],
+            answer: 1,
+            photo: "assets/images/lemon.gif"
+        }, 
+        {
+            question: "How many Grand Slam Titles does Roger Federer have as of 2019?", 
+            choice: ["20", "19", "21", "22" ],
+            answer: 0,
+            
+        },
+        {
+            question: "Who among these players is considered a legend in Tennis?",
+            choice: ["Rod Laver", "Magic Jackson" , "Cristiano Ronaldo" , "Pele"],
+            answer: 0,
+        },
+        {
+            question: "Who is the youngest Grand Slam Womens Singles champion in tennis as of 2019?",
+            choice: ["Monica Seles" , "Tracy Austin" , "Martina Hingis" , "Maria sharapova"],
+            answer: 2,
+        }
+    
+    
+    
+    ];
+    
+    var correctCount = 0;
+    var wrongCount = 0;
+    var unanswerCount = 0;
+    var timer = 10;
+    var intervalId;
+    var userGuess ="";
+    var running = false;
+    var qCount = options.length;
+    var pick;
+    var index;
+    var newArray = [];
+    var holder = [];
+    
+    
+    
+    $("#reset").hide();
+    //click start button to start game
+    $("#start").on("click", function () {
+            $("#start").hide();
+            displayQuestion();
+            runTimer();
+            for(var i = 0; i < options.length; i++) {
+        holder.push(options[i]);
+    }
+        })
+    //timer start
+    function runTimer(){
+        if (!running) {
+        intervalId = setInterval(decrement, 1000); 
+        running = true;
+        }
+    }
+    //timer countdown
+    function decrement() {
+        $("#timeleft").html("<h3>Time remaining: " + timer + "</h3>");
+        timer --;
+    
+        //stop timer if reach 0
+        if (timer === 0) {
+            unanswerCount++;
+            stop();
+            $("#answerblock").html("<p>Time is up! The correct answer is: " + pick.choice[pick.answer] + "</p>");
+            hidepicture();
+        }	
+    }
+    
+    //timer stop
+    function stop() {
+        running = false;
+        clearInterval(intervalId);
+    }
+    //randomly pick question in array if not already shown
+    //display question and loop though and display possible answers
+    function displayQuestion() {
+        //generate random index in array
+        index = Math.floor(Math.random()*options.length);
+        pick = options[index];
+    
+    //	if (pick.shown) {
+    //		//recursive to continue to generate new index until one is chosen that has not shown in this game yet
+    //		displayQuestion();
+    //	} else {
+    //		console.log(pick.question);
+            //iterate through answer array and display
+            $("#questionblock").html("<h2>" + pick.question + "</h2>");
+            for(var i = 0; i < pick.choice.length; i++) {
+                var userChoice = $("<div>");
+                userChoice.addClass("answerchoice");
+                userChoice.html(pick.choice[i]);
+                //assign array position to it so can check answer
+                userChoice.attr("data-guessvalue", i);
+                $("#answerblock").append(userChoice);
+    //		}
+    }
+    
+    
+    
+    //click function to select answer and outcomes
+    $(".answerchoice").on("click", function () {
+        //grab array position from userGuess
+        userGuess = parseInt($(this).attr("data-guessvalue"));
+    
+        //correct guess or wrong guess outcomes
+        if (userGuess === pick.answer) {
+            stop();
+            correctCount++;
+            userGuess="";
+            $("#answerblock").html("<p>Correct!</p>");
+            hidepicture();
+    
+        } else {
+            stop();
+            wrongCount++;
+            userGuess="";
+            $("#answerblock").html("<p>Wrong! The correct answer is: " + pick.choice[pick.answer] + "</p>");
+            hidepicture();
+        }
+    })
+    }
+    
+    
+    function hidepicture () {
+        $("#answerblock").append("<img src=" + pick.photo + ">");
+        newArray.push(pick);
+        options.splice(index,1);
+    
+        var hidpic = setTimeout(function() {
+            $("#answerblock").empty();
+            timer= 10;
+    
+        //run the score screen if all questions answered
+        if ((wrongCount + correctCount + unanswerCount) === qCount) {
+            $("#questionblock").empty();
+            $("#questionblock").html("<h3>Game Over!  Here's how you did: </h3>");
+            $("#answerblock").append("<h4> Correct: " + correctCount + "</h4>" );
+            $("#answerblock").append("<h4> Incorrect: " + wrongCount + "</h4>" );
+            $("#answerblock").append("<h4> Unanswered: " + unanswerCount + "</h4>" );
+            $("#reset").show();
+            correctCount = 0;
+            wrongCount = 0;
+            unanswerCount = 0;
+    
+        } else {
+            runTimer();
+            displayQuestion();
+    
+        }
+        }, 3000);
+    
+    
+    }
+    
+    $("#reset").on("click", function() {
+        $("#reset").hide();
+        $("#answerblock").empty();
+        $("#questionblock").empty();
+        for(var i = 0; i < holder.length; i++) {
+            options.push(holder[i]);
+        }
+        runTimer();
+        displayQuestion();
+    
+    })
+    
+    })
